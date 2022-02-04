061a1b649c839f743d097d7c2c335e1e9723f02a
Oloqq <olgierdpk@gmail.com>
2022-02-04 21:23:06 +0100

    refactor plopping

--- plopping.py
+++ plopping.py
@@ -1,20 +1,22 @@
 # make a plopchart from input image (black-white)
 # the plopchart represents where should the pen plop the paper
-# find a decent order of strokes to fill all the positions on the plopchart
-# convert to gcode
 
 # only pure black pixels (0, 0, 0) are interpreted as painted
 
 from PIL import Image
 import math
 
-brush_um = 2400
+# sample values todo: make configurable
+brush_um = 2400 # um = micrometers
 pixel_to_um = 2400
 plops_per_pixel = math.floor(pixel_to_um / brush_um)
-print('ppp: ', plops_per_pixel)
 
-# pixels, width, height, plops per pixel
-def make_plopchart(pixels, w, h, ppp):
+def make_plopchart(filename, outfile=False, show=False):
+	img = Image.open(filename)
+	pixels = img.load()
+	w, h = img.size
+	ppp = plops_per_pixel
+
 	if ppp < 1:
 		print('ppp < 1 will need some smart interpolation. TBD')
 		exit()
@@ -28,15 +30,14 @@ def make_plopchart(pixels, w, h, ppp):
 				for j in range(ppp):
 					out.putpixel((x*ppp+i, y*ppp+j), val)
 
-	out.save('data/out.png')
-	# out.show()
+	if type(outfile) is str:
+		out.save(outfile)
+	if show: 
+		out.show()
 
 	return out
 
 if __name__ == "__main__":
-	img = Image.open('data/smile.png')
-	pixels = img.load()
-	width, height = img.size
-	make_plopchart(pixels, width, height, plops_per_pixel)
-
+	out = make_plopchart('data/smile.png', outfile='data/out.png', show=False)
+	print(out)
 
