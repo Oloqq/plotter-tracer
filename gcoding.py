5086a91a244a1186c01aa44d02bd4984e181492b
Oloqq <olgierdpk@gmail.com>
2022-02-07 00:08:26 +0100

    enclose gcoding in a class

--- gcoding.py
+++ gcoding.py
@@ -1,4 +1,3 @@
-#temp
 from plopping import make_plopchart
 from navigating import longstroke
 from visualiser import visualize
@@ -6,55 +5,18 @@ from visualiser import visualize
 gcode = ''
 tile_size = 32
 
-# determine experimentally
-z_high = 10 
-z_low = 9
-# 
+# TODO lift the pen in gcode footer
 
-def move(x, y, contact=False):
-	global gcode
-	gcode += f'G{1 if contact else 0} X{x*tile_size} Y{y*tile_size}\n'
-	if x > 22 or y > 22:
-		print(f'!LARGE X/Y!: X:{x} Y:{y}')
-
-def lift():
-	global gcode
-	gcode += f'G0 Z{z_high}\n'
-
-def sink():
-	global gcode
-	gcode += f'G1 Z{z_low}\n'
-
-def encode(moves, save=False):
-	global gcode
-
-	gcode = header
-	
-	pen_down = False
-	for step in moves:
-		print(step)
-		if type(step) is tuple: # it's a displacement
-			x, y = step
-			move(x, y, pen_down)
-		elif type(step) is str: # it's a non-displacement operation 
-			match step:
-				case 'pen up':
-					pen_down = False
-					lift()
-				case 'pen down':
-					pen_down = True
-					sink()
-
-
-	gcode += footer
-
-	if type(save) is str:
-		with open(save, 'w') as f:
-			f.write(gcode)
-
-	return gcode
-
-header = """; I'm already Tracer
+class Gcoder:
+	def __init__(self, settings: dict):
+		self.code: str = ''
+		self.tile_size: int = settings['tile_size']
+		self.pen_down = False
+		# determine experimentally
+		self.z_high = 10 
+		self.z_low = 9
+		# 
+		self.header = """; I'm already Tracer
 M140 S0 ; Bed temperature
 M104 S0 ; Hotend temperature
 M105 ; Report temperatures
@@ -66,20 +28,61 @@ G0 F1000 ; Set movement speed
 
 ; Drawing
 """
-
-footer = """
+		
+		self.footer = """
 ; Footer
 M84 ; Disable all steppers
 """
+
+	def move(self, x, y):
+		self.code += f'G{1 if self.pen_down else 0} X{x*self.tile_size} Y{y*self.tile_size}\n'
+		if x > 22 or y > 22:
+			print(f'!LARGE X/Y!: X:{x} Y:{y}')
+
+	def lift(self):
+		self.pen_down = False
+		self.code += f'G0 Z{self.z_high}\n'
+
+	def dive(self):
+		self.pen_down = True
+		self.code += f'G1 Z{self.z_low}\n'
+
+	def encode(self, moves, save=False):
+		self.code = self.header
+		
+		self.pen_down = False
+		for step in moves:
+			# print(step)
+			if type(step) is tuple: # it's a displacement
+				x, y = step
+				self.move(x, y)
+			elif type(step) is str: # it's a non-displacement operation 
+				match step:
+					case 'pen up':
+						self.lift()
+					case 'pen down':
+						self.dive()
+
+		self.code += self.footer
+
+		if type(save) is str:
+			with open(save, 'w') as f:
+				f.write(gcode)
+
+		return self.code
 
 if __name__ == '__main__':
 	plopchart = make_plopchart('data/smile.png', save=False, show=False)
 	moves = longstroke(plopchart)
 
-
 	# width, height = plopchart.size
 	# the result is saved in data/visualiser_out
 	# visualize(moves, width, height, show=False)
+	settings = {
+		'tile_size': 32
+	}
 
-	gcode = encode(moves, save='data/smile.gcode')
+	gcoder = Gcoder(settings)
+
+	gcode = gcoder.encode(moves, save='data/smile.gcode')
 	print(gcode)
