3f9ee517f9daacbf1fb1909519e56d527af53d81
Olgierd Piofczyk <olgierdpk@gmail.com>
2024-04-06 18:51:17 +0200

    img prep

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,39 @@
+mod image_prep;
+
+use self::image_prep::load_image;
 use std::io::Write;
-use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
 struct Args {
-    img: PathBuf,
+    img_path: String,
+    // output: String,
+    #[structopt(short, long)]
+    converted_img: Option<String>,
 }
+
+// fn find_workpoints() {}
+
+// fn find_workpath() {}
+
+// fn visualize_workpath() {}
+
+// fn encode() {}
 
 fn main() {
     env_logger::Builder::from_default_env()
         .format(|buf, record| writeln!(buf, "{}: {}", record.level(), record.args()))
         .init();
-    let args = Args::from_args();
-    println!("Image: {:?}", args.img);
+    // let args = Args::from_args();
+    let args = Args {
+        img_path: "snek.png".into(),
+        // output: "snek.gcode".into(),
+        converted_img: Some("snek_gray.png".into()),
+    };
+
+    log::info!("Image: {:?}", args.img_path);
+    let img = load_image(args.img_path).unwrap();
+    if let Some(path) = args.converted_img {
+        img.save(path).unwrap();
+    }
 }
